78d95fe8756bf66b51c01f9dc2f4887eef05b210
Eva Gunn <ebcg1@st-andrews.ac.uk@>
2024-08-12 20:24:00 +0100

    adding distributions

--- R/probability_distributions.R
+++ R/probability_distributions.R
@@ -1,3 +1,203 @@
+zero_inflated_lognormal_distribution <- R6Class(
+  "zero_inflated_lognormal_distribution",
+  inherit = greta::.internals$nodes$node_classes$distribution_node,
+  public = list(
+    initialize = function(theta, meanlog, sdlog, dim) {
+      theta <- as.greta_array(theta)
+      meanlog <- as.greta_array(meanlog)
+      sdlog <- as.greta_array(sdlog)
+      # add the nodes as children and parameters
+      dim <- check_dims(theta, meanlog, sdlog, target_dim = dim)
+      self$bounds <- c(0, Inf)
+      
+      super$initialize("zero_inflated_lognormal", dim)
+      self$add_parameter(theta, "theta")
+      self$add_parameter(meanlog, "meanlog")
+      self$add_parameter(sdlog, "sdlog")
+    },
+    
+    tf_distrib = function(parameters, dag) {
+      theta <- parameters$theta
+      meanlog <- parameters$meanlog
+      sdlog <- parameters$sdlog
+      
+      log_prob <- function(x) {
+        tf$where(x == 0, 
+                 {tf$math$log(theta)}, 
+                 {tf$math$log((1-theta)*(exp(-tf$pow(((tf$math$log(x))-meanlog),2)/(fl(2)*tf$pow(sdlog,2)))/(x*sdlog*tf$math$sqrt(fl(2)*tf$cast(tf$constant(pi), tf$float64)))))
+                 })
+      }
+      
+      sample <- function(seed) {
+        
+        binom <- tfp$distributions$Binomial(total_count = 1, probs = theta)
+        lognormal <- tfp$distributions$LogNormal(
+          loc = parameters$meanlog,
+          scale = parameters$sdlog
+        )
+        
+        zi <- binom$sample(seed = seed)
+        lbd <- lognormal$sample(seed = seed)
+        
+        (fl(1) - zi) * lbd
+        
+      }
+      
+      list(log_prob = log_prob, sample = sample, cdf = NULL, log_cdf = NULL)
+    },
+    
+    tf_cdf_function = NULL,
+    tf_log_cdf_function = NULL
+  )
+)
+
+zero_inflated_gamma_distribution <- R6Class(
+  "zero_inflated_gamma_distribution",
+  inherit = greta::.internals$nodes$node_classes$distribution_node,
+  public = list(
+    initialize = function(theta, shape, rate, dim, truncation) {
+      theta <- as.greta_array(theta)
+      shape <- as.greta_array(shape)
+      rate <- as.greta_array(rate)
+      # add the nodes as children and parameters
+      dim <- check_dims(theta, shape, rate, target_dim = dim)
+      self$bounds <- c(0, Inf)
+      super$initialize("zero_inflated_gamma", dim, truncation)
+      self$add_parameter(theta, "theta")
+      self$add_parameter(shape, "shape")
+      self$add_parameter(rate, "rate")
+    },
+    
+    tf_distrib = function(parameters, dag) {
+      theta <- parameters$theta
+      shape <- parameters$shape
+      rate <- parameters$rate
+      
+      # shape = alpha = k
+      # rate = 1/scale = 1/sigma = B
+      log_prob <- function(x) {
+        tf$where(x == 0, 
+                 {tf$math$log(theta)}, 
+                 {tf$math$log((1-theta)*(tf$math$pow(x, shape - 1)*tf$math$exp(-rate*x)*tf$math$pow(rate, shape))/tf$exp(tf$math$lgamma(shape)))
+                 })
+      }
+      
+      sample <- function(seed) {
+        
+        binom <- tfp$distributions$Binomial(total_count = 1, probs = theta)
+        gamma <- tfp$distributions$Gamma(
+          concentration = parameters$shape,
+          rate = parameters$rate
+        )
+        
+        zi <- binom$sample(seed = seed)
+        lbd <- gamma$sample(seed = seed)
+        
+        (fl(1) - zi) * lbd
+        
+      }
+      
+      list(log_prob = log_prob, sample = sample, cdf = NULL, log_cdf = NULL)
+    },
+    
+    tf_cdf_function = NULL,
+    tf_log_cdf_function = NULL
+  )
+)
+
+zero_inflated_poisson_distribution <- R6Class(
+  "zero_inflated_poisson_distribution",
+  inherit = greta::.internals$nodes$node_classes$distribution_node,
+  public = list(
+    initialize = function(theta, lambda, dim) {
+      theta <- as.greta_array(theta)
+      lambda <- as.greta_array(lambda)
+      # add the nodes as children and parameters
+      dim <- check_dims(theta, lambda, target_dim = dim)
+      super$initialize("zero_inflated_poisson", dim, discrete = TRUE)
+      self$add_parameter(theta, "theta")
+      self$add_parameter(lambda, "lambda")
+    },
+    
+    tf_distrib = function(parameters, dag) {
+      theta <- parameters$theta
+      lambda <- parameters$lambda
+      log_prob <- function(x) {
+        
+        tf$math$log(theta * tf$nn$relu(fl(1) - x) + (fl(1) - theta) * tf$pow(lambda, x) * tf$exp(-lambda) / tf$exp(tf$math$lgamma(x + fl(1))))
+      }
+      
+      sample <- function(seed) {
+        
+        binom <- tfp$distributions$Binomial(total_count = 1, probs = theta)
+        pois <- tfp$distributions$Poisson(rate = lambda)
+        
+        zi <- binom$sample(seed = seed)
+        lbd <- pois$sample(seed = seed)
+        
+        (fl(1) - zi) * lbd
+        
+      }
+      
+      list(log_prob = log_prob, sample = sample, cdf = NULL, log_cdf = NULL)
+    },
+    
+    tf_cdf_function = NULL,
+    tf_log_cdf_function = NULL
+  )
+)
+
+
+zero_inflated_negative_binomial_distribution <- R6Class(
+  "zero_inflated_negative_binomial_distribution",
+  inherit = greta::.internals$nodes$node_classes$distribution_node,
+  public = list(
+    initialize = function(theta, size, prob, dim) {
+      theta <- as.greta_array(theta)
+      size <- as.greta_array(size)
+      prob <- as.greta_array(prob)
+      # add the nodes as children and parameters
+      dim <- check_dims(theta, size, prob, target_dim = dim)
+      super$initialize("zero_inflated_negative_binomial", dim, discrete = TRUE)
+      self$add_parameter(theta, "theta")
+      self$add_parameter(size, "size")
+      self$add_parameter(prob, "prob")
+    },
+    
+    tf_distrib = function(parameters, dag) {
+      theta <- parameters$theta
+      size <- parameters$size
+      p <- parameters$prob # probability of success
+      q <- fl(1) - parameters$prob 
+      log_prob <- function(x) {
+        
+        tf$math$log(theta * tf$nn$relu(fl(1) - x) + (fl(1) - theta) * tf$pow(p, size) * tf$pow(q, x) * tf$exp(tf$math$lgamma(x + size)) / tf$exp(tf$math$lgamma(size)) / tf$exp(tf$math$lgamma(x + fl(1))))
+        
+      }
+      
+      sample <- function(seed) {
+        
+        binom <- tfp$distributions$Binomial(total_count = 1, probs = theta)
+        negbin <- tfp$distributions$NegativeBinomial(total_count = size, probs = q) # change of proba / parametrisation in 'stats'
+        
+        zi <- binom$sample(seed = seed)
+        lbd <- negbin$sample(seed = seed)
+        
+        (fl(1) - zi) * lbd
+        
+      }
+      
+      list(log_prob = log_prob, sample = sample, cdf = NULL, log_cdf = NULL)
+    },
+    
+    tf_cdf_function = NULL,
+    tf_log_cdf_function = NULL
+  )
+)
+
+
+
+
 uniform_distribution <- R6Class(
   "uniform_distribution",
   inherit = distribution_node,
@@ -1227,6 +1427,10 @@ lkj_correlation_distribution <- R6Class(
 
 # module for export via .internals
 distribution_classes_module <- module(
+  zero_inflated_poisson_distribution,
+  zero_inflated_negative_binomial_distribution,
+  zero_inflated_lognormal_distribution,
+  zero_inflated_gamma_distribution,
   uniform_distribution,
   normal_distribution,
   lognormal_distribution,
@@ -1602,3 +1806,28 @@ dirichlet_multinomial <- function(size, alpha,
     size, alpha, n_realisations, dimension
   )
 }
+
+#' @rdname distributions
+#' @export
+zero_inflated_poisson <- function (theta, lambda, dim = NULL) {
+  distrib('zero_inflated_poisson', theta, lambda, dim)
+}
+
+#' @rdname distributions
+#' @export
+zero_inflated_negative_binomial <- function (theta, size, prob, dim = NULL) {
+  distrib('zero_inflated_negative_binomial', theta, size, prob, dim)
+}
+
+#' @rdname distributions
+#' @export
+zero_inflated_lognormal <- function(theta, meanlog, sdlog, dim = NULL) {
+  distrib("zero_inflated_lognormal", theta, meanlog, sdlog, dim)
+}
+
+#' @rdname distributions
+#' @export
+zero_inflated_gamma <- function(theta, shape, rate, dim = NULL,truncation = c(0, Inf)) {
+  distrib("zero_inflated_gamma", theta, shape, rate, dim, truncation)
+}
+
